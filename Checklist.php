6895897c3cdd79f323ed9d47d63ffbfb3dfdea45
Dwi Muktianto <dwi.muktianto@gmail.com>
2019-04-08 14:17:46 +0700

    try this temp

---
+++ Checklist.php
@@ -0,0 +1,6 @@
+<?php
+
+// https://uicookies.com/demo/theme/technews/index.html
+// https://uicookies.com/demo/theme/technews/category.html
+// https://uicookies.com/demo/theme/technews/blog.html
+// https://uicookies.com/demo/theme/technews/single.html
---
+++ README.md
@@ -0,0 +1,3 @@
+# Tech News
+
+Updated Tech News
